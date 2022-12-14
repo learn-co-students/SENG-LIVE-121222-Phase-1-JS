5d20dd8a0f586227a7984a283f9948b11a64217b
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2022-12-14 14:43:08 -0800

    end of lesson 3

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -48,12 +48,74 @@ function renderBook(book) {
 
   const btn = document.createElement('button');
   btn.textContent = 'Delete';
+  btn.addEventListener('click', (e) => { 
+    li.remove()
+  })
 
   li.append(h3, pAuthor, pPrice, img, btn);
 
   document.querySelector('#book-list').append(li);
 }
 
+////////////////////////////////////////////////////////////////
+// Event Listeners
+////////////////////////////////////////////////////////////////
+
+const toggleBookFormBtn = document.querySelector('#toggleForm');
+const bookForm = document.querySelector('#book-form')
+
+toggleBookFormBtn.addEventListener('click', (e) => {
+  // const formShowing = !bookForm.classList.toggle('collapsed')
+  // console.log('formShowing', formShowing);
+  // toggleBookFormBtn.textContent = formShowing ? "Hide Book Form" : "New Book";
+  // or
+  const formHidden = bookForm.classList.toggle('collapsed')
+  console.log('formHidden', formHidden);
+  toggleBookFormBtn.textContent = formHidden ?  "New Book" : "Hide Book Form";
+})
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    console.log('you just hit escape!')
+    if (!bookForm.classList.contains('collapsed')) {
+      const formShowing = bookForm.classList.add('collapsed')
+      toggleBookFormBtn.textContent = "New Book";
+    };
+  }
+})
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('submitting');
+  // we need to create a book from the form that looks like this:
+  // {
+  //   id:3,
+  //   title: 'JavaScript: The Good Parts',
+  //   author: 'Douglas Crockford',
+  //   price: 36.00,
+  //   reviews: [{userID: 25, content:'I disagree with everything in this book'}, {userID: 250, content:'Only JS book anyone needs'}],
+  //   inventory: 8,
+  //   imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+  // },
+  // const book = {};
+
+  // book.title = e.target.title.value;
+  // book.author = e.target.author.value;
+  // book.price = parseFloat(e.target.price.value);
+  // book.inventory = parseInt(e.target.inventory.value);
+  // book.imageUrl = e.target.imageUrl.value;
+
+  const book = {
+    title: e.target.title.value,
+    author: e.target.author.value,
+    price: parseFloat(e.target.price.value),
+    inventory: parseInt(e.target.inventory.value),
+    imageUrl: e.target.imageUrl.value
+  };
+  
+  renderBook(book);
+  e.target.reset();
+})
 
 ////////////////////////////////////////////
 // call render functions to populate the DOM
@@ -62,4 +124,3 @@ function renderBook(book) {
 renderHeader(bookStore);
 renderFooter(bookStore);
 bookStore.inventory.forEach(renderBook);
-
